37c91e4f99f7d53b043cf30a1dcea2082e76e626
ShabanaParveen07 <116435795+ShabanaParveen07@users.noreply.github.com>
2024-07-21 11:56:58 +0530

    Add files via upload

---
+++ condition_Pract.py
@@ -0,0 +1,29 @@
+# If condition
+'''a = 5
+
+if a < 10:
+    print("True")
+    print("Ihsaan")
+else:
+    print("false")
+    print("Shabana")
+
+print("condition ends")
+
+#For loop condition
+
+obj =[2,4,6,8]
+for i in obj:
+    print(i*2)'''
+
+# sum of first five natural numbers
+s1 = 0
+for j in range(1, 4):
+    s1 = s1 + j
+    print(s1)
+
+
+
+
+
+
